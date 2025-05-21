38317719234e3bc4638d7dbd0f8f152326f9ba07
Alessandro <abcsandro@hotmail.com>
2025-05-21 15:28:45 -0300

    Removido perguntas padrão

--- data/questions.py
+++ data/questions.py
@@ -4,36 +4,6 @@ from services.moviesService import search_movies
 import random
 
 QUESTIONS = [
-    {
-        'question': 'Qual é o maior planeta do sistema solar?',
-        'options': ['Terra', 'Júpiter', 'Marte', 'Saturno'],
-        'answer': 1
-    },
-    {
-        'question': 'Quantos lados tem um triângulo?',
-        'options': ['3', '4', '5', '6'],
-        'answer': 0
-    },
-    {
-        'question': 'Qual é a capital do Brasil?',
-        'options': ['Rio de Janeiro', 'São Paulo', 'Brasília', 'Salvador'],
-        'answer': 2
-    },
-    {
-        'question': 'Qual é o resultado de 2 + 2?',
-        'options': ['3', '4', '5', '6'],
-        'answer': 1
-    },
-    {
-        'question': 'Qual é o animal conhecido como "Rei da Selva"?',
-        'options': ['Elefante', 'Tigre', 'Leão', 'Gorila'],
-        'answer': 2
-    },
-    {
-        'question': 'Qual é a cor do céu em um dia claro?',
-        'options': ['Verde', 'Azul', 'Amarelo', 'Vermelho'],
-        'answer': 1
-    },
 ]
 
 def get_questions(genre: str = None, decade: str = None, top_n: int = 15):
